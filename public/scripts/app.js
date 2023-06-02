9b72b96bf4c1574a552c7b819c288da69dfab23a
Situ2000 <yuanyuansitu@gmail.com>
2023-06-02 16:26:49 +0000

    Add the jQuery for click the add-task button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+  $('#submit-button').click(function() {
+    var task = $('#input-box').val();
+
+    if (task.trim() === '') {
+      alert("The input is empty, please type something.");
+    } else {
+      var newRow = $('<tr>');
+      newRow.append('<td>' + task + '</td>');
+      newRow.append('<td><button class="edit-button">Edit</button></td>');
+      newRow.append('<td><button class="delete-button">Delete</button></td>');
+
+      $('#list-table').append(newRow);
+      $('#input-box').val('');
+    }
+  });
+});
