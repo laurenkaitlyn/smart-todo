365bf24cb4bbae25e4b8bfafa653481853ec4885
Situ2000 <yuanyuansitu@gmail.com>
2023-06-02 16:49:09 +0000

    Add jQuery to the edit and delete button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,7 @@
 // Client facing scripts here
 
 $(document).ready(function() {
+  // Submit button click event handler
   $('#submit-button').click(function() {
     var task = $('#input-box').val();
 
@@ -15,5 +16,27 @@ $(document).ready(function() {
       $('#list-table').append(newRow);
       $('#input-box').val('');
     }
+  });
+
+  // Edit button click event handler
+  $('#list-table').on('click', '.edit-button', function() {
+    // Get the parent row (tr) of the clicked edit button
+    var row = $(this).closest('tr');
+
+    // Get the task content from the first column of the row
+    var task = row.find('td:first-child').text();
+
+    // Redirect to the edit route with the task ID or other relevant identifier
+    window.location.href = '/api/notes/' + taskId + '/edit';
+  });
+
+
+  // Delete button click event handler
+  $('#list-table').on('click', '.delete-button', function() {
+    // Get the parent row (tr) of the clicked delete button
+    var row = $(this).closest('tr');
+
+    // Remove the entire row from the table
+    row.remove();
   });
 });
