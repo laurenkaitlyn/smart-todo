2d79f4bf9491bde99bab46a132bd2d8bcad6b61c
Situ2000 <yuanyuansitu@gmail.com>
2023-06-03 01:34:52 +0000

    Modify 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,139 +1,54 @@
 // Client facing scripts here
 
 $(document).ready(function() {
-  let selectedCategory = 0;
-
-  $(".category-list-item").click(function() {
-    selectedCategory = $(this).attr("data-id");
-    console.log(selectedCategory);
-    $("body").css("background-image", `url(/images/${selectedCategory}.jpg)`)
-
-    // Filter tasks based on the selected category
-    filterTasks(selectedCategory);
-  });
-
-  function filterTasks(categoryId) {
-    // Show all tasks if the selected category is 'All'
-    if (categoryId === "0") {
-      $('#list-table tr').show();
-    } else {
-      // Hide tasks that don't belong to the selected category
-      $('#list-table tr').each(function() {
-        let taskCategory = $(this).data('task-category');
-        if (taskCategory != categoryId) {
-          $(this).hide();
-        } else {
-          $(this).show();
-        }
-      });
-    }
-  }
-
   // AJAX functions for CRUD operations
+
   function createTask(task) {
-    return $.ajax({
-      url: '/api/notes',
-      method: 'POST',
-      data: { task: task },
-      success: function(response) {
-        // Handle success response
-        console.log('Task created successfully:', response);
-      },
-      error: function(error) {
-        // Handle error response
-        console.error('Error creating task:', error);
-      }
-    });
+    // AJAX request for creating a task
   }
 
   function getAllTasks() {
-    $.ajax({
-      url: '/api/notes',
-      method: 'GET',
-      success: function(response) {
-        // Handle success response
-        console.log('All tasks:', response);
-      },
-      error: function(error) {
-        // Handle error response
-        console.error('Error fetching tasks:', error);
-      }
-    });
+    // AJAX request for fetching all tasks
   }
 
   function getTaskById(taskId) {
-    $.ajax({
-      url: '/api/notes/' + taskId,
-      method: 'GET',
-      success: function(response) {
-        // Handle success response
-        console.log('Task details:', response);
-      },
-      error: function(error) {
-        // Handle error response
-        console.error('Error fetching task details:', error);
-      }
-    });
+    // AJAX request for fetching a task by ID
   }
 
-  // function updateTask(taskId, updatedTask) {
-  //   $.ajax({
-  //     url: '/api/notes/' + taskId + '/edit',
-  //     method: 'POST',
-  //     data: { task: updatedTask },
-  //     success: function(response) {
-  //       // Handle success response
-  //       console.log('Task updated successfully:', response);
-  //     },
-  //     error: function(error) {
-  //       // Handle error response
-  //       console.error('Error updating task:', error);
-  //     }
-  //   });
-  // }
+  function updateTask(taskId, updatedTask) {
+    // AJAX request for updating a task
+  }
 
   function deleteTask(taskId) {
-    $.ajax({
-      url: '/api/notes/' + taskId + '/delete',
-      method: 'POST',
-      success: function(response) {
-        // Handle success response
-        console.log('Task deleted successfully:', response);
-      },
-      error: function(error) {
-        // Handle error response
-        console.error('Error deleting task:', error);
-      }
-    });
+    // AJAX request for deleting a task
   }
 
   // Submit button click event handler
   $('#submit-button').click(function() {
     let task = $('#input-box').val();
-    createTask(task)
-    .then(() => {
-      if (task.trim() === '') {
-        alert("The input is empty, please type something.");
-      } else {
-        let newRow = $('<tr>');
+    createTask(task);
 
-        // Add task cell with checkbox
-        let taskCell = $('<td class="task-cell">');
-        taskCell.append('<input type="checkbox">');
-        taskCell.append(' ');
-        taskCell.append('<label class="task-text">' + task + '</label>');
-        newRow.append(taskCell);
+    if (task.trim() === '') {
+      alert("The input is empty, please type something.");
+    } else {
+      let newRow = $('<tr>');
 
-        // Add edit button cell
-        newRow.append('<td><button class="edit-button">Edit</button></td>');
+      // Add task cell with checkbox
+      let taskCell = $('<td class="task-cell">');
+      taskCell.append('<input type="checkbox">');
+      taskCell.append(' ');
+      taskCell.append('<label class="task-text">' + task + '</label>');
+      newRow.append(taskCell);
 
-        // Add delete button cell
-        newRow.append('<td><button class="delete-button">Delete</button></td>');
+      // Add edit button cell
+      newRow.append('<td><button class="edit-button">Edit</button></td>');
 
-        $('#list-table').append(newRow);
-        $('#input-box').val('');
-      }
-    })
+      // Add delete button cell
+      newRow.append('<td><button class="delete-button">Delete</button></td>');
+
+      $('#list-table').append(newRow);
+      $('#input-box').val('');
+    }
   });
 
   // Fetch all tasks on page load
@@ -144,14 +59,14 @@ $(document).ready(function() {
     // Get the parent row (tr) of the clicked edit button
     let row = $(this).closest('tr');
 
-    // Get the task ID from the row's data attribute
+    // Get the task ID from the row's data attribute or other relevant identifier
     let taskId = row.data('task-id');
 
     // Retrieve task data from the server using the task ID
     getTaskById(taskId)
       .done(function(task) {
-        // Redirect to the edit route with the task ID
-        window.location.href = '/notes/' + taskId;
+        // Redirect to the edit route with the task ID or other relevant identifier
+        window.location.href = '/api/notes/' + taskId + '/edit';
       })
       .fail(function(error) {
         console.error('Error retrieving task:', error);
@@ -191,4 +106,6 @@ $(document).ready(function() {
       }
     }
   });
+
+
 });
